53c560b59e427afd4794fa3d77516a34eccfcc4a
Erkin kyzy Aidai <a73613821@gmail.com>
2024-06-21 19:43:17 +0600

    qwerty

--- handlers/commands.py
+++ handlers/commands.py
@@ -10,19 +10,21 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message):
     description = (
-        "Здравствуйте \n"
-        "Добро пожаловать в инфозаписьбот \n"
-        "/address - Адрес \n"
-        "/contacts - Контакты \n"
-        "/grafic - График работы \n"
-        "/uslugi - Услуги \n"
-        "/zapisatsya - Запись к врачу \n"
-        "/end - Завершение \n"
+        "Здравствуйте!!! \n"
+        "Добро пожаловать в МЕДИНФОЗАПИСЬБОТ!!! \n"
+        "🗺/address - Адрес \n"
+        "☎/contacts - Контакты \n"
+        "⌚/grafic - График работы \n"
+        "📝/uslugi - Услуги \n"
+        "✍/zapisatsya - Запись к врачу \n"
+        "✍/add_client - Добавить клиента \n"
+        "🚫/end - Завершение \n"
     )
     kb = [
         [KeyboardButton(text="/address")], [KeyboardButton(text="/contacts")],
         [KeyboardButton(text="/grafic")], [KeyboardButton(text="/uslugi")], 
-        [KeyboardButton(text="/zapisatsya")],[KeyboardButton(text="/end")],
+        [KeyboardButton(text="/zapisatsya")],[KeyboardButton(text="/add_client")],
+        [KeyboardButton(text="/end")]
     ]
     keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     await message.answer(description, reply_markup=keyboard)
@@ -31,7 +33,7 @@ async def start(message: Message):
 # Обработчик для команды /end
 @router.message(Command(commands=['end']))
 async def end(message: Message):
-    await message.answer("Спасибо, что воспользовались инфозаписьботом! До новых встреч!", show_alert=True)
+    await message.answer("Спасибо, что воспользовались МЕДИНФОЗАПИСЬБОТОМ! До новых встреч!", show_alert=True)
 
 @router.message(Command(commands=['address']))
 async def address(message:Message):
@@ -101,14 +103,14 @@ async def uslugi(message:Message):
     ])
     await message.answer(
     
-        "1.УЛЬТРАЗВУКОВАЯ ДИАГНОСТИКА \n"
-        "2.ТЕРАПИЯ \n"
-        "3.ПСИХОЛОГИЯ КОНСУЛЬТАЦИЯ СЕМЬИ \n"
-        "4.ОРТОПЕДИЯ \n"
-        "5.ЛАБОРАТОРНАЯ ДИАГНОСТИКА АНАЛИЗЫ \n"
-        "6.КОЛОПРОКТОЛОГИЯ \n"
-        "7.ДЕРМАТОВЕНЕРОЛОГИЯ \n"
-        "8.АКУШЕРСТВО И ГИНЕКОЛОГИЯ \n",
+        "1.Ультразвуковая диагностика \n"
+        "2.Терапия \n"
+        "3.Психология консультация семьи \n"
+        "4.Ортопедия \n"
+        "5.Лабороторная диагностика анализы \n"
+        "6.Колопроктология \n"
+        "7.Дерматовенералогия \n"
+        "8.Акушерства и гинекология \n",
         reply_markup=keyboard
     )
 
@@ -174,3 +176,104 @@ async def process_phone_number(message: Message, state: FSMContext):
     await bot_instance.send_message(ADMIN_CHAT_ID, admin_message)
 
     await state.clear()
+
+
+
+
+
+from aiogram.types import Message,CallbackQuery,FSInputFile,ReplyKeyboardRemove
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+class AddClientStates(StatesGroup):
+    first_name = State()
+    last_name = State()
+    age = State()
+    email = State()
+    phone = State()
+    address = State()
+    department_id = State()
+
+
+
+@router.message(Command("add_client"))
+async def cmd_add_client(message:Message,state: FSMContext):
+    await state.set_state(AddClientStates.first_name)
+    await message.answer("Enter first name:",
+    reply_markup=ReplyKeyboardRemove())
+    
+@router.message(Command("cancel"))
+@router.message(F.text.casefold() == "cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info("Cancelling state %r", current_state)
+    await state.clear()
+    await message.answer(
+        "Cancelled.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+
+@router.message(AddClientStates.first_name)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(first_name=message.text)
+    await state.set_state(AddClientStates.last_name)
+    await message.answer("Enter last name:",
+    reply_markup=ReplyKeyboardRemove())
+
+
+@router.message(AddClientStates.last_name)
+async def process_last_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(last_name=message.text)
+    await state.set_state(AddClientStates.age)
+    await message.answer("Enter age:")
+
+
+@router.message(AddClientStates.age)
+async def process_salary(message: Message, state: FSMContext) -> None:
+    await state.update_data(salary=float(message.text))
+    await state.set_state(AddClientStates.email)
+    await message.answer("Enter email:")
+
+@router.message(AddClientStates.email)
+async def process_email(message: Message, state: FSMContext) -> None:
+    await state.update_data(email=message.text)
+    await state.set_state(AddClientStates.phone)
+    await message.answer("Enter phone:")
+
+@router.message(AddClientStates.phone)
+async def process_phone(message: Message, state: FSMContext) -> None:
+    await state.update_data(phone=int(message.text))
+    await state.set_state(AddClientStates.address)
+    await message.answer("Enter address:")
+
+@router.message(AddClientStates.address)
+async def process_address(message: Message, state: FSMContext) -> None:
+    await state.update_data(address=message.text)
+    await state.set_state(AddClientStates.department_id)
+    await message.answer("Enter department id:")
+
+@router.message(AddClientStates.department_id)
+async def process_department_id(message: Message, state: FSMContext) -> None:
+    await state.update_data(department_id=int(message.text))
+    data = await state.get_data()
+    await show_summary(message, data)
+    await state.clear()
+
+async def show_summary(message: Message, data: dict[str, any]):
+    client = Client(first_name=data["first_name"],
+              last_name=data["last_name"],
+              age=data["age"],
+              email=data["email"], 
+              phone=data["phone"], 
+              address=data["address"],
+              department_id=data["department_id"])
+    result = await create_client(client)
+    await message.answer(text="Успешно добавили клиента", reply_markup=ReplyKeyboardRemove())
